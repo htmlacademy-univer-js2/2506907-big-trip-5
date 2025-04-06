c2e574d58ea22bc04d2486bddb846965df013391
Вячеслав <baranov_v_s1@mail.ru>
2025-04-07 00:55:03 +0500

    module2-task1

--- src/view/EditForm.js
+++ src/view/EditForm.js
@@ -1,21 +1,23 @@
 // src/view/EditForm.js
 export default class EditForm {
-  constructor() {
+  constructor(data) {
+    this.data = data; // Данные для редактирования
     this.element = this.createElement();
   }
 
   createElement() {
-    const form = document.createElement('form');
-    form.className = 'edit-form';
-    form.innerHTML = `
-          <h2>Редактировать элемент</h2>
-          <input type="text" placeholder="Новое название" required />
-          <button type="submit">Сохранить</button>
-      `;
-    return form;
-  }
-
-  render() {
-    return this.element;
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <h2>${this.data ? 'Редактировать' : 'Создать'} точку маршрута</h2>
+        <form>
+            <label>Тип: <input type="text" value="${this.data ? this.data.type : ''}" /></label>
+            <label>Город: <input type="text" value="${this.data ? this.data.destination.cityName : ''}" /></label>
+            <label>Описание: <textarea>${this.data ? this.data.destination.description : ''}</textarea></label>
+            <h4>Опции:</h4>
+            <!-- Здесь можно добавить логику для добавления опций -->
+            <button type="submit">Сохранить</button>
+        </form>
+    `;
+    return div;
   }
 }
