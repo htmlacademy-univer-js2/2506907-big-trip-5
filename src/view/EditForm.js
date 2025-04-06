ec80dc996f47f2e9f07af4a24f0d66a841427fe7
Вячеслав <baranov_v_s1@mail.ru>
2025-04-07 01:21:56 +0500

    module4-task1

--- src/view/EditForm.js
+++ src/view/EditForm.js
@@ -1,23 +1,15 @@
 // src/view/EditForm.js
 export default class EditForm {
   constructor(data) {
-    this.data = data; // Данные для редактирования
-    this.element = this.createElement();
+    this.data = data;
   }
 
-  createElement() {
-    const div = document.createElement('div');
-    div.innerHTML = `
-        <h2>${this.data ? 'Редактировать' : 'Создать'} точку маршрута</h2>
-        <form>
-            <label>Тип: <input type="text" value="${this.data ? this.data.type : ''}" /></label>
-            <label>Город: <input type="text" value="${this.data ? this.data.destination.cityName : ''}" /></label>
-            <label>Описание: <textarea>${this.data ? this.data.destination.description : ''}</textarea></label>
-            <h4>Опции:</h4>
-            <!-- Здесь можно добавить логику для добавления опций -->
-            <button type="submit">Сохранить</button>
+  get template() {
+    return `
+        <form class="edit-form">
+            <!-- Поля для редактирования -->
+            <button type="submit">Save</button>
         </form>
     `;
-    return div;
   }
 }
--- src/view/RoutePoint.js
+++ src/view/RoutePoint.js
@@ -1,20 +1,16 @@
 // src/view/RoutePoint.js
 export default class RoutePoint {
   constructor(data) {
-      this.data = data; // Данные точки маршрута
-      this.element = this.createElement();
+    this.data = data;
   }
 
-  createElement() {
-    const div = document.createElement('div');
-    div.innerHTML = `
-        <h3>${this.data.type}</h3>
-        <p>${this.data.destination.cityName}: ${this.data.destination.description}</p>
-        <h4>Опции:</h4>
-        <ul>
-            ${this.data.options.map(option => <li>${option.name} - ${option.price} руб.</li>).join('')}
-        </ul>
+  get template() {
+    return `
+        <div class="route-point">
+            <h3>${this.data.type}</h3>
+            <p>${this.data.destination.cityName}: ${this.data.destination.description}</p>
+            <button class="edit-button">Edit</button>
+        </div>
     `;
-    return div;
   }
 }
